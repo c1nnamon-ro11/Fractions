79f250381564e5e5eacd63b462698c18a90137b8
c1nnamon-ro11 <vladislaw.vlad2@gmail.com>
2020-09-27 18:40:47 +0300

    Initial commit

---
+++ Fraction.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+
+namespace Fractals
+{
+    class Fraction
+    {
+        //Fields
+        int numerator;
+        int denominator;
+        int integer;
+        string sign;
+
+        //Constructor
+        public Fraction()
+        {
+            numerator = 1;
+            denominator = 1;
+            integer = 0;
+        }
+
+        public Fraction(string number)
+        {
+            //Exception (wrong fractal data)
+                numerator = int.Parse(number.Split('/')[0]);
+                denominator = int.Parse(number.Split('/')[1]);
+                //Simplification fraction sign
+                if (denominator < 0) {
+                    denominator = -denominator;
+                    numerator = -numerator;
+                }
+            if (denominator == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+        }
+
+        static int CommonDedominator(int denominatorOfFirstFraction, int denominatorOfSecondFraction)
+        {
+            int CommonDedominator;
+            int newDenominator;
+            int multiplier = 2; //counter than increases if newDenominator isn`t result
+
+            //if denominators of fractions are multiples
+            if (denominatorOfFirstFraction > denominatorOfSecondFraction)
+            {
+                CommonDedominator = denominatorOfFirstFraction;
+                if (CommonDedominator % denominatorOfSecondFraction == 0) return CommonDedominator;
+            }
+            else
+            {
+                CommonDedominator = denominatorOfSecondFraction;
+                if (CommonDedominator % denominatorOfFirstFraction == 0) return CommonDedominator;
+            }
+
+            //if denominators of fractions are not multiples
+            while (true)
+            {
+                newDenominator = CommonDedominator * multiplier;
+                if (newDenominator % denominatorOfFirstFraction == 0 && newDenominator % denominatorOfSecondFraction == 0)
+                {
+                    return newDenominator;
+                }
+                multiplier++;
+            }
+        }
+        //Four operations
+        public Fraction PlusFractals(Fraction second)
+        {
+            Fraction answer = new Fraction();
+            answer.denominator = CommonDedominator(denominator, second.denominator);
+            answer.numerator = numerator * (answer.denominator / denominator) + second.numerator * (answer.denominator / second.denominator);
+            return answer;
+        }
+
+        public Fraction MinusFractals(Fraction second)
+        {
+            Fraction answer = new Fraction();
+            answer.denominator = CommonDedominator(denominator, second.denominator);
+            answer.numerator = numerator * (answer.denominator / denominator) - second.numerator * (answer.denominator / second.denominator);
+            return answer;
+        }
+        public Fraction MultyFractals(Fraction second)
+        {
+            Fraction answer = new Fraction();
+            answer.denominator = denominator * second.denominator;
+            answer.numerator = numerator * second.numerator;
+            return answer;
+        }
+        public Fraction DivideFractals(Fraction second)
+        {
+            Fraction answer = new Fraction();
+            answer.denominator = denominator * second.numerator;
+            answer.numerator = numerator * second.denominator;
+            return answer;
+        }
+
+        //Largest Common Divisor for fraction simplification 
+        public int LargestCommonDivisor(int numerator, int denominator)
+        {
+            while (numerator != denominator)
+            {
+                if (numerator == 0)
+                    break;
+                if (numerator > denominator)
+                    numerator = numerator - denominator;
+                else denominator = denominator - numerator;
+            }
+            return numerator;
+        }
+
+
+        public void Simplification()
+        {
+            int div = LargestCommonDivisor(Math.Abs(numerator), Math.Abs(denominator));
+            numerator/= div;
+            denominator/= div;
+            if (numerator < 0 && denominator < 0) { numerator = -numerator; denominator = -denominator; }
+        }
+
+        //If  you need mixed number
+        public void IntegerPart()
+        {
+            if (numerator < 0) sign = "-";
+            else sign = "+";
+            numerator = Math.Abs(numerator);
+            while (numerator >= denominator)
+            {
+                integer++;
+                numerator -= denominator;
+            }
+        }
+
+        public override string ToString()
+        {
+            Simplification();
+            IntegerPart();
+            if (integer == 0 && numerator == 0) return "0";
+            if (integer == 0) return sign + numerator + "/" + denominator;
+            if (numerator == 0) return sign + integer;
+            return sign + integer + " " + numerator + "/" + denominator;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Fractals
+{  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter data: ");
+                string[] str = Console.ReadLine().Split(' ');
+                //str[0] - first fraction
+                //str[1] - operation
+                //str[2] - second fraction
+                //Wrong data format
+                if (str.Length != 3) throw new Exception();
+
+                Fraction firstFraction = new Fraction(str[0]);
+                Fraction secondFraction = new Fraction(str[2]);
+                Fraction answerFraction;
+
+                switch (str[1]) //Operations            
+                {
+                    case "+":
+                        answerFraction = firstFraction.PlusFractals(secondFraction);
+                        Console.WriteLine(answerFraction.ToString());
+                        break;
+                    case "-":
+                        answerFraction = firstFraction.MinusFractals(secondFraction);
+                        Console.WriteLine(answerFraction.ToString());
+                        break;
+                    case "*":
+                        answerFraction = firstFraction.MultyFractals(secondFraction);
+                        Console.WriteLine(answerFraction.ToString());
+                        break;
+                    case "/":
+                        answerFraction = firstFraction.DivideFractals(secondFraction);
+                        Console.WriteLine(answerFraction.ToString());
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect operation!");
+                        break;
+                }
+                Console.WriteLine("Want repeat?: y/n");
+                if(Console.ReadLine() !="y")
+                break;
+                Console.Clear();
+            }
+            Console.ReadLine();
+        }
+    }
+}
